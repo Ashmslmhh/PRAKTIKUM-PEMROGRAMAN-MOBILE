5c5ed86246619766af8a8e27fd5621844f4fc197
Ashmslmhh <ashmasalimah@gmail.com>
2026-04-05 13:18:39 +0800

    Modul 1 - Dice Roller

---
+++ app/src/main/java/com/example/prakmodul1/XmlActivity.kt
@@ -0,0 +1,47 @@
+package com.example.prakmodul1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class XmlActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_xml)
+
+        val dice1 = findViewById<ImageView>(R.id.imageViewDice1)
+        val dice2 = findViewById<ImageView>(R.id.imageViewDice2)
+        val button = findViewById<Button>(R.id.button)
+        val message = findViewById<TextView>(R.id.textViewMessage)
+
+        button.setOnClickListener {
+            val result1 = (1..6).random()
+            val result2 = (1..6).random()
+
+            dice1.setImageResource(when (result1) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            })
+
+            dice2.setImageResource(when (result2) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            })
+
+            message.text = if (result1 == result2) "Selamat, anda dapat dadu double!"
+            else "Anda belum beruntung!"
+
+            message.visibility = TextView.VISIBLE
+        }
+    }
+}
